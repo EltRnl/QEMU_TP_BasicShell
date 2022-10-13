7d3d60fcf613d6815d742210bb604773b75eeb05
Elliot Renel <elliotrenel@gmail.com>
2022-10-13 21:41:49 +0200

    Adding some Globals for Step 1 done

--- arm.boot/main.c
+++ arm.boot/main.c
@@ -4,6 +4,16 @@
  * This is the C entry point, upcalled once the hardware has been setup properly
  * in assembly language, see the reset.s file.
  */
+void* test0;
+void* test1;
+void* test2 = 14;
+void* test3;
+void* test4 = 1456;
+void* test5;
+void* test6;
+void* test7;
+void* test8;
+void* test9 = 42;
 
 void _start() {
   int i = 0;
